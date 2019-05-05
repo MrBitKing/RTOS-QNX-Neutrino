4ff616c95d5c1bd6a2d84a287c3a9dac8ff4b7e4
BitcoinKing <nanangassa@yahoo.fr>
2019-05-05 18:02:09 -0400

    .gitignore fix

---
+++ cst8244_Lab5/lab5_inputs/build/x86_64-debug/src/assign1_inputs.d
@@ -0,0 +1,2 @@
+build/x86_64-debug/src/assign1_inputs.o: src/assign1_inputs.c \
+ src/../../lab5_controller/src/proj.h
---
+++ cst8244_lab04/lab4_server/build/x86_64-debug/src/lab4_server.d
@@ -0,0 +1,2 @@
+build/x86_64-debug/src/lab4_server.o: src/lab4_server.c \
+ src/calc_message.h
